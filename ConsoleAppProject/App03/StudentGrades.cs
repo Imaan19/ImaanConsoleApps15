14edff49b7a88e692cd77c84e126ff1a6987b861
Imaan19 <Imaanmajid@icloud.com>
2023-03-05 14:10:04 +0000

    Added methods and student properties

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -11,6 +11,76 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
+        public string[] Students { get; set; }
 
+        public int[] Marks { get; set; }
+
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+        //
+        public StudentGrades()
+        {
+            Students = new string[]
+                    {
+                "Daniel", "Dylan", "Eric",
+                "Georgia", "Hasan", "Hamza",
+                "Jack", "Liam", "Shan", "Shamial"
+                    };
+
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        public void CalculateStats()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        public void CalculateGradeProfile()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
